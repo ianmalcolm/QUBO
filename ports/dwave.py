02a7d4fc981b02a58b0ae08b9509cedc11728b56
Xu Jun <>
2019-12-19 10:21:20 +0800

    try connectedcomponentscomposite

--- ports/dwave.py
+++ ports/dwave.py
@@ -1,6 +1,7 @@
 from .solver import Solver
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+
 import dimod
 import utils.index as idx
 import numpy as np
@@ -15,7 +16,7 @@ class Dwave(Solver):
             returns: a solution
                     solution is a tuple (dict, float) representing sample and energy.
         '''
-        print("Dwave starts solving...")
+        print("solver starts the process...")
         mtx = matrix.copy()
         var_id = 'x'
         size = mtx.shape[0]
@@ -27,7 +28,7 @@ class Dwave(Solver):
         linear = {}
         quadratic = {}
 
-        print("Dwave is processing matrix to upper-triangular...")
+        print("solver is preparing coefficient dict.")
         for i in range(size):
             for j in range(size):
                 id_i = idx.var_str(var_id,i)
@@ -38,13 +39,15 @@ class Dwave(Solver):
                     linear[id_i] = mtx[i][i]
                 elif i<j:
                     quadratic[(id_i, id_j)] = mtx[i][j]
+        print("solver is constructing bqm.")
         bqm = dimod.BinaryQuadraticModel(
             linear=linear,
             quadratic=quadratic,
             offset=0,
             vartype=dimod.BINARY)
-        print("Dwave starts annealing...")
-        response = EmbeddingComposite(DWaveSampler()).sample(bqm, num_reads=1000)
+        print("Solver engages Dwave quantum hardware!")
+        sampler = dimod.ConnectedComponentsComposite(EmbeddingComposite(DWaveSampler()))
+        response = sampler.sample(bqm, num_reads=1000))
         for sample, energy, num_occurrences in response.data():
             print(sample, "Energy: ", energy, "Occurrences: ", num_occurrences)
 
