f9e1c276535c49b11a04f2e5f5d1bec5d1d5c0fe
Xu Jun <>
2019-12-19 11:10:14 +0800

    try splitting into 2 groups: emable import dwave

--- ports/dwave.py
+++ ports/dwave.py
@@ -1,6 +1,6 @@
 from .solver import Solver
-#from dwave.system.samplers import DWaveSampler
-#from dwave.system.composites import EmbeddingComposite
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
 
 import dimod
 import utils.index as idx
