f60b05aa7740e09c7c3a75faf70578a465c5e449
xj <willxujun@hotmail.com>
2020-02-05 17:52:00 +0800

    1.90

--- orders/order_gen_test.py
+++ orders/order_gen_test.py
@@ -1,6 +1,6 @@
 from order_gen import OrderGen
 
-order_generator = OrderGen(10,10)
+order_generator = OrderGen(15,15)
 
-o = order_generator.generate_exact(24)
+o = order_generator.generate_exact(64)
 order_generator.save("order.txt")
