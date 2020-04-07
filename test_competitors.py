553d3f218f7d6a7377717f268b3101da3065681c
xujun <willxujun@hotmail.com>
2020-04-07 23:10:12 +0800

    1.0

--- test_competitors.py
+++ test_competitors.py
@@ -1,7 +1,9 @@
 import os
 import math
 import numpy as np
+import pandas as pd
 from datetime import datetime
+import json
 
 from orders.order_parser import OrderParser
 from DistanceGenerator import DistanceGenerator
@@ -18,15 +20,8 @@ from sim.test_route import RouteEvaluator
 import utils.mtx as mtx
 
 RESULT_FOLDER = "simdata"
-
-NUM_SKUS = 5
-WAREHOUSE_NUM_COLS = 16
-WAREHOUSE_NUM_ROWS = 4
-NUM_LOCS = WAREHOUSE_NUM_COLS*WAREHOUSE_NUM_ROWS
-NUM_GROUPS = 8
-DIST_VERTICAL = 1
-DIST_HORIZONTAL = 3
 ORDER_DIRNAME = 'orders'
+CONFIG_DIRNAME = 'configs'
 
 group_num_cols = 2
 group_num_rows = 4
@@ -34,24 +29,48 @@ group_num_locs = group_num_cols * group_num_rows
 
 NUM_ITERATIONS = 10
 
-def main():
-    test_abc = []
-    test_coi = []
-    test_ifhoos = []
-    for i in range(NUM_ITERATIONS):
-        order_filename = 'order'+str(i)+'.txt'
-        order_name = 'order'+str(i)
-        order_path = os.path.join(ORDER_DIRNAME,order_filename)
-        str_abc, str_coi, str_ifhoos = run(order_path, order_name)
-        test_abc.append(str_abc)
-        test_coi.append(str_coi)
-        test_ifhoos.append(str_ifhoos)
-    print(test_abc)
-    print(test_coi)
-    print(test_ifhoos)
+TAKE = ['order_270_30_a.txt']
 
-def run(order_path, order_filename):
+def main():
+    for filename in os.listdir(ORDER_DIRNAME):
+        if filename in TAKE:
+            order_character = filename.split('_')
+            num_items = num_locs = int(order_character[1])
+            num_skus = int(order_character[2])
+            
+            config_filename = os.path.join(CONFIG_DIRNAME, 'config'+str(num_items)+'_'+str(num_skus)+'.json')
+            with open(config_filename, 'r') as f:
+                warehouse_config = json.load(f)
+            
+            for i in range(1):
+                result_dict_list = run(filename, warehouse_config)
+                result_file = os.path.join(RESULT_FOLDER, filename+'_competitors.csv')
+                if os.path.exists(result_file):
+                    df = pd.read_csv(result_file,index_col=0)
+                else:
+                    df = pd.DataFrame()
+                
+                new_result = postprocess(result_dict_list)
+                df = df.append(new_result, ignore_index=True)
+                df.to_csv(result_file)
+
+def run(order_filename,config):
+    NUM_SKUS = int(config['NUM_SKUS'])
+    WAREHOUSE_NUM_COLS = int(config['WAREHOUSE_NUM_COLS'])
+    WAREHOUSE_NUM_ROWS = int(config['WAREHOUSE_NUM_ROWS'])
+    NUM_LOCS = int(config['NUM_LOCS'])
+    NUM_GROUPS = int(config['NUM_GROUPS'])
+    DIST_VERTICAL = int(config['DIST_VERTICAL'])
+    DIST_HORIZONTAL = int(config['DIST_HORIZONTAL'])
+    GROUP_NUM_ROWS = int(config['GROUP_NUM_ROWS'])
+    GROUP_NUM_COLS = int(config['GROUP_NUM_COLS'])
+    EXHAUST_AGGREGATES = config['EXHAUST_AGGREGATES']
+    RANDOM_BUNCHING = config['RANDOM_BUNCHING']
+    RANDOM_GROUPING = config['RANDOM_GROUPING']
+
+    order_path = os.path.join(ORDER_DIRNAME,order_filename)
     order_parser = OrderParser(order_path, NUM_SKUS, threshold=0)
+    
     F = order_parser.gen_F()
     qty = order_parser.summary()
 
@@ -69,8 +88,8 @@ def run(order_path, order_filename):
     order_parser = OrderParser(order_path, NUM_SKUS, threshold=0)
     order_set = order_parser.gen_raw_orders()
 
-    ifhoos = IFHOOS(F,D)
-    sol_ifhoos = ifhoos.run()
+    # ifhoos = IFHOOS(F,D)
+    # sol_ifhoos = ifhoos.run()
 
     abc = ABCMethod(NUM_LOCS, NUM_LOCS, np.diag(F), np.diag(D_euclidean), 8)
     sol_abc = abc.run()
@@ -85,17 +104,17 @@ def run(order_path, order_filename):
         WAREHOUSE_NUM_COLS,
         WAREHOUSE_NUM_ROWS,
         DIST_VERTICAL,
-        DIST_HORIZONTAL,
+        3,
         NUM_LOCS,
         NUM_LOCS
     )
     res_abc = evaluator.run(sol_abc)
     res_coi = evaluator.run(sol_coi)
-    res_ifhoos = evaluator.run(sol_ifhoos)
+    # res_ifhoos = evaluator.run(sol_ifhoos)
     
     print(mtx.from_mtx_to_map(sol_abc))
     print(mtx.from_mtx_to_map(sol_coi))
-    print(mtx.from_mtx_to_map(sol_ifhoos))
+    # print(mtx.from_mtx_to_map(sol_ifhoos))
     
     str_abc = "result of abc is " + str(res_abc) + '\n'
     print(str_abc)
@@ -103,13 +122,20 @@ def run(order_path, order_filename):
     str_coi = "result of coi is " + str(res_coi) + '\n'
     print(str_coi)
     
-    str_ifhoos = "result of ifhoos is " + str(res_ifhoos) + '\n'
-    print(str_ifhoos)
+    # str_ifhoos = "result of ifhoos is " + str(res_ifhoos) + '\n'
+    # print(str_ifhoos)
 
-    print(np.diag(D))
-    print(np.diag(D_euclidean))
+    # print(np.diag(D))
+    # print(np.diag(D_euclidean))
+    result_dict = {}
+    result_dict['abc'] = res_abc
+    result_dict['coi'] = res_coi
+    # result_dict['ifhoos'] = res_ifhoos
 
-    return res_abc, res_coi, res_ifhoos
+    return [result_dict]
+
+def postprocess(result_dict):
+    return pd.DataFrame(result_dict)
 
 if __name__ == "__main__":
     main()
